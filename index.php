a666d4fcec7d9f76e506821d6f2f6b5df2d977d3
Mustafa Ozan Alpay <frozsgy@gmail.com>
2019-02-07 16:47:33 +0300

    init

--- index.php
+++ index.php
@@ -2,16 +2,8 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>The world is changed by your example</title>
+<title>#OurRights</title>
 <link href="./css/msic.css" rel="stylesheet" type="text/css" />
-<!-- 
-International Human Rights Day Poster Maker
-Coded and designed by Mustafa Ozan ALPAY
-TurkMSIC NTSDD, December 2013
-If you have any questions and suggestions please feel free to contact me by frozsgy [at] gmail [dot] com
-
-Special thanks to Mustafa Temizyurek and Monika Szamosova
--->
 </head>
 
 <body>
@@ -20,12 +12,21 @@ Special thanks to Mustafa Temizyurek and Monika Szamosova
 <div class="bosdiv"></div>
 <div class="ana">
 <div class="ana2" id="esas">
-  <p>Welcome to the website of the Human Rights Day poster campaign!</p>
-  <p>Human Rights Day  is on December 10th. On this website you can easily create a poster with the photo of your choice. This poster can then be used as a print or Facebook display picture - whichever option you use to express your support to human rights.</p>
-  <p>Feel free to share this website with everyone you know and in you profile picture description!</p>
-  <p>This is an initiative of the Standing Committee on Human Rights and Peace of the Turkish Medical Students' International Committee and the Standing Committee on Human Rights and Peace of the International Federation of Medical Students' Associations.</p>
-  <p>The world is changed by your example.</p>
-   <div id="fb-root"></div>
+<h2>Welcome to the website of The IFMSA World Human Rights Day 2016 Profile Picture Page!</h2>
+<p>World Human Rights Day (WHRD) is annually celebrated on December 10th, which commemorates the adoption of the United Nation’s Universal Declaration of Human Rights in 1948. This step was the breaking point in human rights recognition. Human rights were created to fulfill our basic rights and freedoms as human beings. They include our right to live, our right to education, health, freedom of religion, freedom of speech and thoughts, and equal rights for everyone all around the world.</p>
+<p>WHRD is an opportunity for medical students to connect together to promote, protect and advocate for equal rights for everyone. WHRD campaign already has its tradition in SCORP, having organized WHRD campaigns annually since 2013. This year, we aim to further increase the impact of the campaign with more influential activities. WHRD event also intends to attract medical students to show active support and participation in SCORP activities.</p>
+<p>With gratitude to The Turkish Medical Students' International Committee, we hereby present the WHRD Profile Picture page. Use this website to adjust your Facebook Profile Picture, in support of human rights. Choose one of the human rights, create your new profile picture and show your stand! By sharing this poster and the link to this website with your friends, you will help us in raising awareness on human rights. Let the world know that you support human rights.</p>
+<p>#HumanRightsDay2016 - because we have to stand up together to protect #OurRights</p>
+<p>For more information: <a href=https://www.facebook.com/groups/36162662121//>IFMSA SCORP Facebook Group</a> and <a href=https://issuu.com/ifmsa/docs/scorp_whrd_2016_manual_draft>The WHRD 2016 Manual.</a></p>
+<p>
+<?php
+$fi = new FilesystemIterator("uploads/", FilesystemIterator::SKIP_DOTS);
+$total_fi = iterator_count($fi);
+$thisyear_fi = $total_fi - 3546 ;
+echo $total_fi." posters created so far! ".$thisyear_fi." of them were created for this year's campaign!";
+?>
+</p>
+<div id="fb-root"></div>
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
@@ -35,25 +36,52 @@ Special thanks to Mustafa Temizyurek and Monika Szamosova
 }(document, 'script', 'facebook-jssdk'));</script><div class="fb-like" data-href="http://hrd.turkmsic.net" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div>
 <a href="https://twitter.com/share" class="twitter-share-button" data-text="&quot;The world is changed by your example.&quot; Human Rights Day - " data-via="turkmsic" data-related="ifmsa" data-hashtags="HumanRightsDay">Tweet</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-
 <div class="g-plusone" data-size="medium"></div><script>(function() {var po = document.createElement("script"); po.type = "text/javascript"; po.async = true;po.src = "https://apis.google.com/js/plusone.js";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(po, s);})();</script>
-
 <script src="//platform.linkedin.com/in.js" type="text/javascript"></script><script type="IN/Share" data-counter="right"></script>
-  
-  <hr />
-  <form action="temiz.php" method="post" enctype="multipart/form-data" name="form1" target="_self">
-    <p>Create your poster: 
-    <input type="file" name="foto" id="foto" accept="image/*">
-    <label>
-      <input name="scorplogo" type="checkbox" id="scorplogo" value="1">
-      With SCORP Logo</label>
- 
-    <input type="submit" name="button" id="button" value="Upload">
-  </p>
-    <p>Supported formats: jpeg, png, gif</p>
-  </form>
-  <img src="./img/ornek.jpg?p=futura" alt="Example" width="910">
- 
+<hr />
+<form action="temiz.php" method="post" enctype="multipart/form-data" name="form1" target="_self">
+  <p>Create your poster:
+  <input type="file" name="foto" id="foto" accept="image/*"><p>
+  <p>
+    <select name="stamp" id="stamp">
+    <option value="1">#1 Right to Equality</option>
+    <option value="2">#2 Freedom from Discrimination</option>
+    <option value="3">#3 Right to Life, Liberty, Personal Security</option>
+    <option value="4">#4 Freedom from Slavery</option>
+    <option value="5">#5 Freedom from Torture and Degrading Treatment</option>
+    <option value="6">#6 Right to Recognition as a Person before the Law</option>
+    <option value="7">#7 Right to Equality before the Law</option>
+    <option value="8">#8 Right to Remedy by Competent Tribunal</option>
+    <option value="9">#9 Freedom from Arbitrary Arrest and Exile</option>
+    <option value="10">#10 Right to Fair Public Hearing</option>
+    <option value="11">#11 Right to be Considered Innocent until Proven Guilty</option>
+    <option value="12">#12 Freedom from Interference with Privacy, Family, Home and Correspondence</option>
+    <option value="13">#13 Right to Free Movement in and out of the Country</option>
+    <option value="14">#14 Right to Asylum in other Countries from Persecution</option>
+    <option value="15">#15 Right to a Nationality and the Freedom to Change It</option>
+    <option value="16">#16 Right to Marriage and Family</option>
+    <option value="17">#17 Right to Own Property</option>
+    <option value="18">#18 Freedom of Belief and Religion</option>
+    <option value="19">#19 Freedom of Opinion and Information</option>
+    <option value="20">#20 Right of Peaceful Assembly and Association</option>
+    <option value="21">#21 Right to Participate in Government and in Free Elections</option>
+    <option value="22">#22 Right to Social Security</option>
+    <option value="23">#23 Right to Desirable Work and to Join Trade Unions</option>
+    <option value="24">#24 Right to Rest and Leisure</option>
+    <option value="25">#25 Right to Adequate Living Standard</option>
+    <option value="26">#26 Right to Education</option>
+    <option value="27">#27 Right to Participate in the Cultural Life of Community</option>
+    <option value="28">#28 Right to a Social Order that Articulates this Document</option>
+    <option value="29">#29 Community Duties Essential to Free and Full Development</option>
+    <option value="30">#30 Freedom from State or Personal Interference in the above Rights</option>
+    </select>
+</p><p>
+  <input type="submit" name="button" id="button" value="Upload">
+</p>
+  <p>Supported formats: jpeg, png, gif</p>
+  <p>Works best on square formatted images. Otherwise, it will be resized and alligned to the middle.<p>
+</form>
+<center><img src="./img/ornek.jpg" alt="Example" width="700"></center> 
 </div>
 </div>
 <div class="bosdiv"></div>
--- temiz.php
+++ temiz.php
@@ -2,17 +2,8 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>The world is changed by your example</title>
+<title>#OurRights</title>
 <link href="./css/msic.css" rel="stylesheet" type="text/css" />
-<!-- 
-International Human Rights Day Poster Maker
-Coded and designed by Mustafa Ozan ALPAY
-TurkMSIC NTSDD, December 2013
-If you have any questions and suggestions please feel free to contact me by frozsgy [at] gmail [dot] com
-
-Special thanks to Mustafa Temizyurek and Monika Szamosova
--->
-</head>
 </head>
 
 <body>
@@ -21,78 +12,79 @@ Special thanks to Mustafa Temizyurek and Monika Szamosova
 <div class="bosdiv"></div>
 <div class="ana">
 <div class="ana2" id="esas">
-  
-  
+
 <?php
-//Get the uploaded picture 
 $foto=@$_FILES['foto']['tmp_name'];
-//Die if the picture is not jpeg, gif or png, or too large
-if ((($_FILES["foto"]["type"] == "image/gif") || ($_FILES["foto"]["type"] == "image/jpeg") || ($_FILES["foto"]["type"] == "image/jpg") || ($_FILES["foto"]["type"] == "image/pjpeg") || ($_FILES["foto"]["type"] == "image/x-png") || ($_FILES["foto"]["type"] == "image/png")) && ($_FILES["foto"]["size"] > 10000)) { 
-// Load the stamp and the photo to apply the watermark to
-$stamp = imagecreatefrompng('stamp.png');
-$stamp2 = imagecreatefrompng('stamp2.png');
-$im = imagecreatefromjpeg($foto);
-$imagex=imagesx($im);
-$imagey=imagesy($im);
-//Resize the picture
-if($imagex > $imagey) {
-	//if the image is horizontal
-	$kucultmeorani=880/$imagey;
-	$kucukx=$kucultmeorani*$imagex;
-	$kucuky=880;
+if ((($_FILES["foto"]["type"] == "image/gif") || ($_FILES["foto"]["type"] == "image/jpeg") || ($_FILES["foto"]["type"] == "image/jpg") || ($_FILES["foto"]["type"] == "image/pjpeg") || ($_FILES["foto"]["type"] == "image/x-png") || ($_FILES["foto"]["type"] == "image/png")) && ($_FILES["foto"]["size"] > 10000)) {
+  switch ($_POST['stamp']) {
+  case "1": $stamp2 = imagecreatefrompng('1.png'); break;
+  case "2": $stamp2 = imagecreatefrompng('2.png'); break;
+  case "3": $stamp2 = imagecreatefrompng('3.png'); break;
+  case "4": $stamp2 = imagecreatefrompng('4.png'); break;
+  case "5": $stamp2 = imagecreatefrompng('5.png'); break;
+  case "6": $stamp2 = imagecreatefrompng('6.png'); break;
+  case "7": $stamp2 = imagecreatefrompng('7.png'); break;
+  case "8": $stamp2 = imagecreatefrompng('8.png'); break;
+  case "9": $stamp2 = imagecreatefrompng('9.png'); break;
+  case "10": $stamp2 = imagecreatefrompng('10.png'); break;
+  case "11": $stamp2 = imagecreatefrompng('11.png'); break;
+  case "12": $stamp2 = imagecreatefrompng('12.png'); break;
+  case "13": $stamp2 = imagecreatefrompng('13.png'); break;
+  case "14": $stamp2 = imagecreatefrompng('14.png'); break;
+  case "15": $stamp2 = imagecreatefrompng('15.png'); break;
+  case "16": $stamp2 = imagecreatefrompng('16.png'); break;
+  case "17": $stamp2 = imagecreatefrompng('17.png'); break;
+  case "18": $stamp2 = imagecreatefrompng('18.png'); break;
+  case "19": $stamp2 = imagecreatefrompng('19.png'); break;
+  case "20": $stamp2 = imagecreatefrompng('20.png'); break;
+  case "21": $stamp2 = imagecreatefrompng('21.png'); break;
+  case "22": $stamp2 = imagecreatefrompng('22.png'); break;
+  case "23": $stamp2 = imagecreatefrompng('23.png'); break;
+  case "24": $stamp2 = imagecreatefrompng('24.png'); break;
+  case "25": $stamp2 = imagecreatefrompng('25.png'); break;
+  case "26": $stamp2 = imagecreatefrompng('26.png'); break;
+  case "27": $stamp2 = imagecreatefrompng('27.png'); break;
+  case "28": $stamp2 = imagecreatefrompng('28.png'); break;
+  case "29": $stamp2 = imagecreatefrompng('29.png'); break;
+  case "30": $stamp2 = imagecreatefrompng('30.png'); break;
+  }
+
+  switch ($_FILES['foto']['type']) {
+  	case 'image/gif': $im = imagecreatefromgif($foto); break;
+  	case 'image/png': $im = imagecreatefrompng($foto); break;
+  	case 'image/x-png': $im = imagecreatefrompng($foto); break;
+  	default: $im = imagecreatefromjpeg($foto); break;
+  }
+  $imagex=imagesx($im);
+  $imagey=imagesy($im);
+  if($imagex > $imagey) {
+  	$kucultmeorani=800/$imagex;
+  	$kucukx=800;
+  	$kucuky=$kucultmeorani*$imagey;
+  	}
+  else {
+  	$kucultmeorani=800/$imagey;
+  	$kucukx=$kucultmeorani*$imagex;
+  	$kucuky=800;
+
+  }
+  $marge_right = 0;
+  $marge_bottom = 0;
+  $white=imagecreatefromjpeg('stamp0.jpg');
+  $norm = imagecreatetruecolor(800, 800);
+  imagecopy($norm, $white, 0, 0, 0, 0, 800,800);
+  $ortax = (800-$kucukx) / 2;
+  $ortay = (800-$kucuky) / 2;
+  imagecopyresampled($norm, $im, $ortax, $ortay, 0,0,$kucukx, $kucuky, $imagex, $imagey);
+  imagecopy($norm, $stamp2, 0,0, 0, 0, 800, 800);
+  $ip=$_SERVER['REMOTE_ADDR'];
+  $nome=time().md5($ip).rand(1,999).'.jpg';
+  imagejpeg($norm, 'uploads/'.$nome, 100);
+  imagedestroy($norm);
+  echo '<p>Your poster is ready to share! You can save the picture below or you can use this link to share your picture: <input type="text" readonly="readonly" value="http://hrd.turkmsic.net/uploads/'.$nome.'" onClick="this.select();"> <br /><a href="index.php">Create a new poster</a> </p><p>';
+  echo '<center><img src="uploads/'.$nome.'" width="700"></center></p>';
 }
 else {
-	//if the image is vertical
-	$kucultmeorani=850/$imagex;
-	$kucukx=850;
-	$kucuky=$kucultmeorani*$imagey;	
-}
-//Convert it to black and white
-imagefilter($im, IMG_FILTER_GRAYSCALE);
-//imagefilter($im, IMG_FILTER_CONTRAST, -100);
-
-// Set the margins for the stamp and get the height/width of the stamp image
-$marge_right = 0;
-$marge_bottom = 0;
-$sx = imagesx($stamp);
-$sy = imagesy($stamp);
-
-// Copy the stamp image onto our photo using the margin offsets and the photo 
-// width to calculate positioning of the stamp. 
-    
-	//create an empty image file
-	$dst = imagecreatetruecolor($kucukx, $kucuky);
-	//copy our black & white picture to the empty image file and resize it
-    imagecopyresampled($dst, $im, 0, 0, 0, 0, $kucukx, $kucuky, $imagex, $imagey);
-//check if we have the scorp logo checked on the upload page
-$scorplogovarmi=@$_POST['scorplogo'];
-if($scorplogovarmi == '1') {
-//we have it, so let's paste it
-
-imagecopy($dst, $stamp, 0, 0, 0, 0, imagesx($stamp), imagesy($stamp));
-
-}
-//finally add the image that has the text we want
-imagecopy($dst, $stamp2, $kucukx - imagesx($stamp2) - $marge_right, $kucuky - imagesy($stamp2) - $marge_bottom, 0, 0, imagesx($stamp2), imagesy($stamp2));
-
-// Output and free memory
-//header('Content-type: image/jpeg');
-
-//create a random & unique name so we can save our image succesfully
-//get the ip address of the uploader
-$ip=$_SERVER['REMOTE_ADDR'];
-//create a name with the timestamp, md5 of the ip and a random number
-$nome=time().md5($ip).rand(1,999).'.jpg';
-//create a jpeg file of the image and save it
-imagejpeg($dst, 'uploads/'.$nome);
-//remove the image from the cache so we won't have memory problems
-imagedestroy($dst);
-//voila! give the image to the user
-echo '<p>Your poster is ready to share! You can save the picture below or you can use this link to share your picture: <input type="text" readonly="readonly" value="http://hrd.turkmsic.net/uploads/'.$nome.'" onClick="this.select();"> <br /><a href="index.php">Create a new poster</a> </p><p>';
-echo '<img src="uploads/'.$nome.'" width="910"></p>';
-}
-else {
-	//the image has problems
 	printf('<p>Please provide a valid picture. </p><p><a href="index.php">Back</a></p>');
 }
 ?>
