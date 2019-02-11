a8942c8f670dd4ef2acbd3b0a201352b54e1efc6
Mustafa Ozan Alpay <frozsgy@gmail.com>
2019-02-11 17:05:25 +0300

    shrh version

--- index.php
+++ index.php
@@ -2,28 +2,28 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>#OurRights</title>
+<title>#SRHRday</title>
 <link href="./css/msic.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
 <div class="bosdiv"></div>
-<div id="logo"><div class="temiz"><img src="./img/scorp.png" alt="SCORP Logo"></div></div>
+<div id="logo"><div class="temiz"><img src="./img/scora.png" alt="SCORA Logo"></div></div>
 <div class="bosdiv"></div>
 <div class="ana">
 <div class="ana2" id="esas">
-<h2>Welcome to the website of The IFMSA World Human Rights Day 2016 Profile Picture Page!</h2>
-<p>World Human Rights Day (WHRD) is annually celebrated on December 10th, which commemorates the adoption of the United Nation’s Universal Declaration of Human Rights in 1948. This step was the breaking point in human rights recognition. Human rights were created to fulfill our basic rights and freedoms as human beings. They include our right to live, our right to education, health, freedom of religion, freedom of speech and thoughts, and equal rights for everyone all around the world.</p>
-<p>WHRD is an opportunity for medical students to connect together to promote, protect and advocate for equal rights for everyone. WHRD campaign already has its tradition in SCORP, having organized WHRD campaigns annually since 2013. This year, we aim to further increase the impact of the campaign with more influential activities. WHRD event also intends to attract medical students to show active support and participation in SCORP activities.</p>
-<p>With gratitude to The Turkish Medical Students' International Committee, we hereby present the WHRD Profile Picture page. Use this website to adjust your Facebook Profile Picture, in support of human rights. Choose one of the human rights, create your new profile picture and show your stand! By sharing this poster and the link to this website with your friends, you will help us in raising awareness on human rights. Let the world know that you support human rights.</p>
-<p>#HumanRightsDay2016 - because we have to stand up together to protect #OurRights</p>
-<p>For more information: <a href=https://www.facebook.com/groups/36162662121//>IFMSA SCORP Facebook Group</a> and <a href=https://issuu.com/ifmsa/docs/scorp_whrd_2016_manual_draft>The WHRD 2016 Manual.</a></p>
+<h2>Welcome to the website of The IFMSA Sexual and Reproductive Health and Rights Awareness Day 2019 Profile Picture Page!</h2>
+<p>On February 12th we celebrate the International Day for Sexual and Reproductive Health and Rights (SRHR) Awareness. We are raising awareness towards the importance of SRHR and helping everyone understand why it is vital for everyone and how it can positively affect our lives. </p>
+<p>We invite you to change your profile frame to any of the options presented below. Each frame goes with an example of why we all should care about SRHR, raising our voices all together to make them be heard.</p>
+<p>Choose the cause you care about as your frame here(LINK to this page) and use this caption! Also, invite everyone to join us in this campaign. SRHR matters to everyone!</p>
+<p> hashtag and motto here --> #HumanRightsDay2016 - because we have to stand up together to protect #OurRights</p>
+<p>For more information: <a href="https://www.facebook.com/groups/23117698977/" target="_blank">IFMSA SCORA Facebook Group</a>.</p>
 <p>
 <?php
 $fi = new FilesystemIterator("uploads/", FilesystemIterator::SKIP_DOTS);
 $total_fi = iterator_count($fi);
-$thisyear_fi = $total_fi - 3546 ;
-echo $total_fi." posters created so far! ".$thisyear_fi." of them were created for this year's campaign!";
+#$thisyear_fi = $total_fi - 3546 ;
+echo $total_fi." posters created so far! "; #.$thisyear_fi." of them were created for this year's campaign!";
 ?>
 </p>
 <div id="fb-root"></div>
@@ -44,36 +44,18 @@ echo $total_fi." posters created so far! ".$thisyear_fi." of them were created f
   <input type="file" name="foto" id="foto" accept="image/*"><p>
   <p>
     <select name="stamp" id="stamp">
-    <option value="1">#1 Right to Equality</option>
-    <option value="2">#2 Freedom from Discrimination</option>
-    <option value="3">#3 Right to Life, Liberty, Personal Security</option>
-    <option value="4">#4 Freedom from Slavery</option>
-    <option value="5">#5 Freedom from Torture and Degrading Treatment</option>
-    <option value="6">#6 Right to Recognition as a Person before the Law</option>
-    <option value="7">#7 Right to Equality before the Law</option>
-    <option value="8">#8 Right to Remedy by Competent Tribunal</option>
-    <option value="9">#9 Freedom from Arbitrary Arrest and Exile</option>
-    <option value="10">#10 Right to Fair Public Hearing</option>
-    <option value="11">#11 Right to be Considered Innocent until Proven Guilty</option>
-    <option value="12">#12 Freedom from Interference with Privacy, Family, Home and Correspondence</option>
-    <option value="13">#13 Right to Free Movement in and out of the Country</option>
-    <option value="14">#14 Right to Asylum in other Countries from Persecution</option>
-    <option value="15">#15 Right to a Nationality and the Freedom to Change It</option>
-    <option value="16">#16 Right to Marriage and Family</option>
-    <option value="17">#17 Right to Own Property</option>
-    <option value="18">#18 Freedom of Belief and Religion</option>
-    <option value="19">#19 Freedom of Opinion and Information</option>
-    <option value="20">#20 Right of Peaceful Assembly and Association</option>
-    <option value="21">#21 Right to Participate in Government and in Free Elections</option>
-    <option value="22">#22 Right to Social Security</option>
-    <option value="23">#23 Right to Desirable Work and to Join Trade Unions</option>
-    <option value="24">#24 Right to Rest and Leisure</option>
-    <option value="25">#25 Right to Adequate Living Standard</option>
-    <option value="26">#26 Right to Education</option>
-    <option value="27">#27 Right to Participate in the Cultural Life of Community</option>
-    <option value="28">#28 Right to a Social Order that Articulates this Document</option>
-    <option value="29">#29 Community Duties Essential to Free and Full Development</option>
-    <option value="30">#30 Freedom from State or Personal Interference in the above Rights</option>
+    <option value="1">#1 Safe Abortion</option>
+    <option value="2">#2 STI Testing</option>
+    <option value="3">#3 Sexual Identity</option>
+    <option value="4">#4 Pleasurable Sex Life</option>
+    <option value="5">#5 Sex Education</option>
+    <option value="6">#6 No HIV Stigma</option>
+    <option value="7">#7 Maternal Care</option>
+    <option value="8">#8 No Female Genital Mutilation</option>
+    <option value="9">#9 Contraception</option>
+    <option value="10">#10 Cervical Cancer</option>
+    <option value="11">#11 Breast Cancer</option>
+    <option value="12">#12 Prostate Cancer</option>
     </select>
 </p><p>
   <input type="submit" name="button" id="button" value="Upload">
@@ -81,7 +63,7 @@ echo $total_fi." posters created so far! ".$thisyear_fi." of them were created f
   <p>Supported formats: jpeg, png, gif</p>
   <p>Works best on square formatted images. Otherwise, it will be resized and alligned to the middle.<p>
 </form>
-<center><img src="./img/ornek.jpg" alt="Example" width="700"></center> 
+<center><img src="./img/ornek.jpg" alt="Example" width="700"></center>
 </div>
 </div>
 <div class="bosdiv"></div>
--- temiz.php
+++ temiz.php
@@ -2,13 +2,13 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>#OurRights</title>
+<title>#SRHRday</title>
 <link href="./css/msic.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
 <div class="bosdiv"></div>
-<div id="logo"><div class="temiz"><img src="./img/scorp.png" alt="SCORP Logo"></div></div>
+<div id="logo"><div class="temiz"><img src="./img/scora.png" alt="SCORA Logo"></div></div>
 <div class="bosdiv"></div>
 <div class="ana">
 <div class="ana2" id="esas">
@@ -17,36 +17,18 @@
 $foto=@$_FILES['foto']['tmp_name'];
 if ((($_FILES["foto"]["type"] == "image/gif") || ($_FILES["foto"]["type"] == "image/jpeg") || ($_FILES["foto"]["type"] == "image/jpg") || ($_FILES["foto"]["type"] == "image/pjpeg") || ($_FILES["foto"]["type"] == "image/x-png") || ($_FILES["foto"]["type"] == "image/png")) && ($_FILES["foto"]["size"] > 10000)) {
   switch ($_POST['stamp']) {
-  case "1": $stamp2 = imagecreatefrompng('./stamps/1.png'); break;
-  case "2": $stamp2 = imagecreatefrompng('./stamps/2.png'); break;
-  case "3": $stamp2 = imagecreatefrompng('./stamps/3.png'); break;
-  case "4": $stamp2 = imagecreatefrompng('./stamps/4.png'); break;
-  case "5": $stamp2 = imagecreatefrompng('./stamps/5.png'); break;
-  case "6": $stamp2 = imagecreatefrompng('./stamps/6.png'); break;
-  case "7": $stamp2 = imagecreatefrompng('./stamps/7.png'); break;
-  case "8": $stamp2 = imagecreatefrompng('./stamps/8.png'); break;
-  case "9": $stamp2 = imagecreatefrompng('./stamps/9.png'); break;
-  case "10": $stamp2 = imagecreatefrompng('./stamps/10.png'); break;
-  case "11": $stamp2 = imagecreatefrompng('./stamps/11.png'); break;
-  case "12": $stamp2 = imagecreatefrompng('./stamps/12.png'); break;
-  case "13": $stamp2 = imagecreatefrompng('./stamps/13.png'); break;
-  case "14": $stamp2 = imagecreatefrompng('./stamps/14.png'); break;
-  case "15": $stamp2 = imagecreatefrompng('./stamps/15.png'); break;
-  case "16": $stamp2 = imagecreatefrompng('./stamps/16.png'); break;
-  case "17": $stamp2 = imagecreatefrompng('./stamps/17.png'); break;
-  case "18": $stamp2 = imagecreatefrompng('./stamps/18.png'); break;
-  case "19": $stamp2 = imagecreatefrompng('./stamps/19.png'); break;
-  case "20": $stamp2 = imagecreatefrompng('./stamps/20.png'); break;
-  case "21": $stamp2 = imagecreatefrompng('./stamps/21.png'); break;
-  case "22": $stamp2 = imagecreatefrompng('./stamps/22.png'); break;
-  case "23": $stamp2 = imagecreatefrompng('./stamps/23.png'); break;
-  case "24": $stamp2 = imagecreatefrompng('./stamps/24.png'); break;
-  case "25": $stamp2 = imagecreatefrompng('./stamps/25.png'); break;
-  case "26": $stamp2 = imagecreatefrompng('./stamps/26.png'); break;
-  case "27": $stamp2 = imagecreatefrompng('./stamps/27.png'); break;
-  case "28": $stamp2 = imagecreatefrompng('./stamps/28.png'); break;
-  case "29": $stamp2 = imagecreatefrompng('./stamps/29.png'); break;
-  case "30": $stamp2 = imagecreatefrompng('./stamps/30.png'); break;
+    case "1": $stamp2 = imagecreatefrompng('stamps/01.png'); break;
+    case "2": $stamp2 = imagecreatefrompng('stamps/02.png'); break;
+    case "3": $stamp2 = imagecreatefrompng('stamps/03.png'); break;
+    case "4": $stamp2 = imagecreatefrompng('stamps/04.png'); break;
+    case "5": $stamp2 = imagecreatefrompng('stamps/05.png'); break;
+    case "6": $stamp2 = imagecreatefrompng('stamps/06.png'); break;
+    case "7": $stamp2 = imagecreatefrompng('stamps/07.png'); break;
+    case "8": $stamp2 = imagecreatefrompng('stamps/08.png'); break;
+    case "9": $stamp2 = imagecreatefrompng('stamps/09.png'); break;
+    case "10": $stamp2 = imagecreatefrompng('stamps/10.png'); break;
+    case "11": $stamp2 = imagecreatefrompng('stamps/11.png'); break;
+    case "12": $stamp2 = imagecreatefrompng('stamps/12.png'); break;
   }
 
   switch ($_FILES['foto']['type']) {
@@ -58,30 +40,30 @@ if ((($_FILES["foto"]["type"] == "image/gif") || ($_FILES["foto"]["type"] == "im
   $imagex=imagesx($im);
   $imagey=imagesy($im);
   if($imagex > $imagey) {
-  	$kucultmeorani=800/$imagex;
-  	$kucukx=800;
+  	$kucultmeorani=1024/$imagex;
+  	$kucukx=1024;
   	$kucuky=$kucultmeorani*$imagey;
   	}
   else {
-  	$kucultmeorani=800/$imagey;
+  	$kucultmeorani=1024/$imagey;
   	$kucukx=$kucultmeorani*$imagex;
-  	$kucuky=800;
+  	$kucuky=1024;
 
   }
   $marge_right = 0;
   $marge_bottom = 0;
-  $white=imagecreatefromjpeg('stamp0.jpg');
-  $norm = imagecreatetruecolor(800, 800);
-  imagecopy($norm, $white, 0, 0, 0, 0, 800,800);
-  $ortax = (800-$kucukx) / 2;
-  $ortay = (800-$kucuky) / 2;
+  $white=imagecreatefrompng('stamps/logos.png');
+  $norm = imagecreatetruecolor(1024, 1024);
+  $ortax = (1024-$kucukx) / 2;
+  $ortay = (1024-$kucuky) / 2;
   imagecopyresampled($norm, $im, $ortax, $ortay, 0,0,$kucukx, $kucuky, $imagex, $imagey);
-  imagecopy($norm, $stamp2, 0,0, 0, 0, 800, 800);
+  imagecopy($norm, $white, 0, 0, 0, 0, 1024,1024);
+  imagecopy($norm, $stamp2, 0,0, 0, 0, 1024, 1024);
   $ip=$_SERVER['REMOTE_ADDR'];
   $nome=time().md5($ip).rand(1,999).'.jpg';
   imagejpeg($norm, 'uploads/'.$nome, 100);
   imagedestroy($norm);
-  echo '<p>Your poster is ready to share! You can save the picture below or you can use this link to share your picture: <input type="text" readonly="readonly" value="http://hrd.turkmsic.net/uploads/'.$nome.'" onClick="this.select();"> <br /><a href="index.php">Create a new poster</a> </p><p>';
+  echo '<p>Your poster is ready to share! You can save the picture below or you can use this link to share your picture: <input type="text" readonly="readonly" value="http://'.$_SERVER['HTTP_HOST'].substr($_SERVER['REQUEST_URI'], 0, -9).'uploads/'.$nome.'" onClick="this.select();"> <br /><a href="index.php">Create a new poster</a> </p><p>';
   echo '<center><img src="uploads/'.$nome.'" width="700"></center></p>';
 }
 else {
